e6b58ce17c0ac8660c36b595d8de7f9f476362c5
Patrick <patrick@dragonarmy.com>
2016-07-13 15:35:57 -0400

    Added Javascript to create board

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,8 +4,19 @@ var cardThree = "king";
 var cardFour = "king";
 console.log("JS file is connected to HTML! Woo!")
 
-if (cardOne === cardTwo || cardFour === cardFour) {
-	alert ("You found a match!");
-} else {
-	alert ("Sorry try again.")
+// if (cardOne === cardTwo || cardFour === cardFour) {
+// 	alert ("You found a match!");
+// } else {
+// 	alert ("Sorry try again.")
+// }
+function createBoard(numCards) {
+	var gameBoard = document.getElementById('game-board');
+
+	for (i = 0; i < numCards; i++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
+	}
 }
+
+createBoard(4);
